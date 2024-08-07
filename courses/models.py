7ab8213b7d9fae6e32d8e3031f79eb724c9ac924
anur1 <ahmettonuri@gmail.com>
2024-08-07 10:42:12 +0300

    first commit

---
+++ courses/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+    name = models.CharField( max_length=50)
+    slug = models.SlugField(default= "", null=False, unique=True, db_index = True, max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    #imageUrl = models.CharField(max_length=50, blank=False)
+    image = models.FileField(upload_to="images", default ="")  #yüklenen dosyaları uploads/images klasörüne kaydeder. ImageField çalışmazsa FileField kullanılabilir. 
+    date = models.DateField(auto_now=True)
+    isActive = models.BooleanField(default=False)
+    isHome = models.BooleanField(default = False)
+    slug = models.SlugField(default = "", null=False, blank=True,  unique=True, db_index=True)
+    #category = models.ForeignKey(Category, default = 1, on_delete=models.CASCADE, related_name="kurslar")
+    categories = models.ManyToManyField(Category,)
+    #def save(self, *args, **kwargs):
+        #self.slug=slugify(self.title)
+        #super().save(args, kwargs)
+
+    def __str__(self):
+        return f"{self.title}"
+    
+
+class UploadModel (models.Model):
+    image = models.FileField(upload_to="images")
